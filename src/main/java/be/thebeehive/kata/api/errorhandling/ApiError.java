bef4831e1c47c7a3fdea15afdb73ca7416bfab9a
lucas <gepeoverload@gmail.com>
2022-10-16 10:30:06 +0200

    added error handling for bad requests - missing body - and made the test pass

---
+++ src/main/java/be/thebeehive/kata/api/errorhandling/ApiError.java
@@ -0,0 +1,32 @@
+package be.thebeehive.kata.api.errorhandling;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiError {
+
+    private HttpStatus status;
+    private String message;
+
+    public ApiError(HttpStatus status, String message) {
+        super();
+        this.status = status;
+        this.message = message;
+
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/be/thebeehive/kata/api/errorhandling/CustomRestApiExceptionHandler.java
@@ -0,0 +1,26 @@
+package be.thebeehive.kata.api.errorhandling;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomRestApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request){
+
+        final ApiError apiError = new ApiError(status,"Invalid request. Verify all required data is included");
+
+        return new ResponseEntity(apiError, headers, status);
+
+    }
+
+}
