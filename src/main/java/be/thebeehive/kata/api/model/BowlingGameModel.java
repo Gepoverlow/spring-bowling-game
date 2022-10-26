3719d8ca349aa77c18b72aa4642797b6ce56c961
lucas <gepeoverload@gmail.com>
2022-10-26 11:56:53 +0200

    removed all 'this.' from the bowling game model

--- src/main/java/be/thebeehive/kata/api/model/BowlingGameModel.java
+++ src/main/java/be/thebeehive/kata/api/model/BowlingGameModel.java
@@ -2,7 +2,6 @@ package be.thebeehive.kata.api.model;
 
 import be.thebeehive.kata.api.dto.RollDto;
 import be.thebeehive.kata.api.errorhandling.exception.IllegalSumOfRollsInFrameException;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +11,6 @@ import java.util.List;
 @Getter
 @Setter
 //TODO: PUBLIC methods need to go on top
-//TODO: remove 'this' keyword where not needed
 public class BowlingGameModel {
 
     //TODO: review these variable naming and utility
@@ -39,7 +37,7 @@ public class BowlingGameModel {
 
         for(int i = 0; i < GAME_FRAMES; i++){
 
-            this.gameFrames.add(new FrameModel());
+            gameFrames.add(new FrameModel());
 
         }
 
@@ -48,9 +46,9 @@ public class BowlingGameModel {
     //TODO: 3 things : refactor for loop, conditionals are too messy and encapsulation (FrameModel) is being broken
     private void handleExtrasForSpareFrames(RollDto roll){
 
-        for(int i = 0 ; i < this.gameFrames.size() ; i++){
+        for(int i = 0 ; i < gameFrames.size() ; i++){
 
-            FrameModel currentFrame = this.gameFrames.get(i);
+            FrameModel currentFrame = gameFrames.get(i);
 
             if(currentFrame.isSpare() && currentFrame.getExtraRolls().isEmpty() && currentFrame.getInitialRolls().size() == MAX_NOT_LAST_FRAME_SIZE){
 
@@ -66,9 +64,9 @@ public class BowlingGameModel {
     //TODO: 3 things : refactor for loop, conditionals are too messy and encapsulation (FrameModel) is being broken
     private void handleExtrasForStrikeFrames(RollDto roll){
 
-        for(int i = 0 ; i < this.gameFrames.size() ; i++) {
+        for(int i = 0 ; i < gameFrames.size() ; i++) {
 
-            FrameModel currentFrame = this.gameFrames.get(i);
+            FrameModel currentFrame = gameFrames.get(i);
 
             if(currentFrame.isFrameOpenForStrikeRolls() && currentFrame.getExtraRolls().isEmpty()) {
 
@@ -88,9 +86,9 @@ public class BowlingGameModel {
     //TODO: method naming -> is this name really describing what it does? Also Update loop
     private void tagAllFrames(){
 
-        for(int i = 0 ; i < this.gameFrames.size() ; i++) {
+        for(int i = 0 ; i < gameFrames.size() ; i++) {
 
-            this.gameFrames.get(i).tagFrame();
+            gameFrames.get(i).tagFrame();
 
         }
 
@@ -101,15 +99,15 @@ public class BowlingGameModel {
 
         int sum = 0;
 
-        for(int i = 0; i < this.gameFrames.size(); i++){
+        for(int i = 0; i < gameFrames.size(); i++){
 
-           int individualFrameScore = this.gameFrames.get(i).calculateFinalFrameValue();
+           int individualFrameScore = gameFrames.get(i).calculateFinalFrameValue();
 
             sum = sum + individualFrameScore;
 
         }
 
-        this.score = sum;
+        score = sum;
 
     }
 
@@ -130,12 +128,12 @@ public class BowlingGameModel {
 
     private void checkIfGameIsOver(){
 
-        FrameModel lastGameFrame = this.gameFrames.get(this.gameFrames.size() - 1);
+        FrameModel lastGameFrame = gameFrames.get(gameFrames.size() - 1);
 
         //TODO: this conditional statement is WAY to big and unreadable: maybe we can handle this on a separate private method?
         if((lastGameFrame.isSpare() && !lastGameFrame.isFrameOpenForSpareRolls()) || (lastGameFrame.isStrike() && !lastGameFrame.isFrameOpenForStrikeRolls()) || (!lastGameFrame.isSpare() && !lastGameFrame.isStrike() && !lastGameFrame.isFrameOpenForInitialRolls() ) ){
 
-            this.setGameOver(true);
+            setGameOver(true);
 
         }
 
@@ -144,13 +142,13 @@ public class BowlingGameModel {
     //TODO: this method needs an overhaul: looping this many times is not performant. Loops, statements and the likes need to be cleaner
    public void handleScoreCalculation(RollDto roll){
 
-        for(int i = 0 ; i < this.gameFrames.size() ; i++){
+        for(int i = 0 ; i < gameFrames.size() ; i++){
 
-            if(this.gameFrames.get(i).isFrameOpenForInitialRolls()){
+            if(gameFrames.get(i).isFrameOpenForInitialRolls()){
 
-                FrameModel currentFrame = this.gameFrames.get(i);
+                FrameModel currentFrame = gameFrames.get(i);
 
-                if(this.checkIfFrameTotalWouldGoOverMaximum(currentFrame, roll)){
+                if(checkIfFrameTotalWouldGoOverMaximum(currentFrame, roll)){
 
                     throw new IllegalSumOfRollsInFrameException("The sum of the rolls in this frame is illegal, please select a lower number of pins");
 
@@ -172,13 +170,13 @@ public class BowlingGameModel {
         }
 
         //TODO: this looks super ugly
-        this.handleExtrasForSpareFrames(roll);
-        this.handleExtrasForStrikeFrames(roll);
+        handleExtrasForSpareFrames(roll);
+        handleExtrasForStrikeFrames(roll);
 
-        this.calculateGameScore();
-        this.tagAllFrames();
+        calculateGameScore();
+        tagAllFrames();
 
-        this.checkIfGameIsOver();
+        checkIfGameIsOver();
 
    }
 
