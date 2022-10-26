26918be6cf4a2973db9fb1ca580c5fd9c36fb10e
lucas <gepeoverload@gmail.com>
2022-10-26 11:48:22 +0200

    added test for the mapper

---
+++ src/test/java/be/thebeehive/kata/api/mapper/BowlingGameMapperTest.java
@@ -0,0 +1,35 @@
+package be.thebeehive.kata.api.mapper;
+
+import be.thebeehive.kata.api.dto.CreateGameDto;
+import be.thebeehive.kata.api.model.BowlingGameModel;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = BowlingGameMapperImpl.class)
+public class BowlingGameMapperTest {
+
+    private static final Faker FAKER = Faker.instance();
+
+    @Autowired
+    BowlingGameMapper bowlingGameMapper;
+
+    @Test
+    public void shouldMapCreateDtoToModel(){
+
+        String name = FAKER.starTrek().character();
+        CreateGameDto createGameDto = new CreateGameDto(name);
+        BowlingGameModel bowlingGameModel = bowlingGameMapper.createBowlingGameDtoToBowlingGameModel(createGameDto);
+        assertThat(bowlingGameModel.getName()).isEqualTo(name);
+        assertThat(bowlingGameModel.getScore()).isEqualTo(0);
+        assertThat(bowlingGameModel.getGameId()).isNotNull();
+
+    }
+
+}
